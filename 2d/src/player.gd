5366db9e997e02d6f1d1f7f38300d13384f75130
adflixit <adflixit@gmail.com>
2024-04-08 20:01:44 +0300

    Update player.gd

--- 2d/src/player.gd
+++ 2d/src/player.gd
@@ -6,45 +6,40 @@ var gravity = 1440
 var jump_force = gravity * 1.0
 var bounce_force = gravity * 0.8
 var trace_reach = 400
-var bounce_sigma = deg2rad(20)
+var bounce_sigma =  PI / 8
 
-var trace_time = 0
 var trace_queried = false
-var trace_angle = 0.0
+var trace_vector = Vector2()
 var velocity = Vector2()
 
 
 func _physics_process(delta):
 	delta *= A.game.time_scale
+	velocity.y += gravity * delta
 	
 	if trace_queried:
 		trace_queried = false
 		
+		var from = trace_vector * A.player_radius
+		var to = from + trace_vector * trace_reach
 		var space_state = get_world_2d().direct_space_state
-		var angle = Vector2(cos(trace_angle), sin(trace_angle))
-		var from = angle * A.player_radius
-		var to = from + angle * trace_reach
 		var trace = space_state.intersect_ray(global_position + from, global_position + to)
 		
 		if !trace.empty():
 			var fraction = (global_position + from).distance_to(trace.position) / trace_reach
-			var push_angle = Vector2(cos(trace_angle + PI), sin(trace_angle + PI))
-			velocity = push_angle * (jump_force * (1.0 - fraction))
-	
-	velocity.y += gravity * delta
+			var jump_vector = -trace_vector
+			velocity = jump_vector * (jump_force * (1.0 - fraction))
 	
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		var angle = collision.normal.angle()
-		var bounce_mult = 1.0 if angle < 0 else 0.4
-		var bounce_angle = angle - PI/2 + rand_range(bounce_sigma, PI - bounce_sigma)
-		var bounce_factor = bounce_force * bounce_mult
-		
-		velocity = (Vector2(cos(bounce_angle), sin(bounce_angle)) * bounce_factor)
-		
 		var platform = collision.collider as Platform
 		if platform:
-			platform.flinch(collision.position, collision.normal * bounce_factor)
+			platform.flinch(collision.position, velocity)
+		
+		var normal = collision.normal
+		var bounce_vector = normal.rotated(PI / 2 - rand_range(bounce_sigma, PI - bounce_sigma))
+		var bounce_factor = bounce_force if normal.y < 0 else bounce_force * 0.4
+		velocity = bounce_vector * bounce_factor
 	
 	if position.y > A.game.pos.y + A.vp_height * 2:
 		A.game.destroy_player()
@@ -52,9 +47,7 @@ func _physics_process(delta):
 
 func _unhandled_input(event):
 	if (!A.game.is_paused and
-		trace_time + 100 < Time.get_ticks_msec() and
-		((event is InputEventScreenTouch and event.pressed) or
-		(event is InputEventMouseButton and event.pressed))):
-			trace_time = Time.get_ticks_msec()
+		(event is InputEventScreenTouch and event.pressed) or
+		(event is InputEventMouseButton and event.pressed)):
 			trace_queried = true
-			trace_angle = (global_position - (A.camera.position + event.position)).angle() + PI
+			trace_vector = -(global_position - (A.camera.position + event.position)).normalized()
